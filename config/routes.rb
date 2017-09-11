4b408d6ec235a10d0a9518983f4bb2b3abff0f34
Gus <augustin.holguin@gmail.com>
2017-09-11 16:56:45 +0000

    fullname fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :path=>'',
-    :path_name=>{:sign_in=>'login', :sign_out=>'logout', :edit=>'profile'}
+    :path_names=>{:sign_in=>'login', :sign_out=>'logout', :edit=>'profile'}
   root 'pages#home'
 end
--- db/migrate/20170911151025_add_fullname.rb
+++
@@ -1,4 +0,0 @@
-class AddFullname < ActiveRecord::Migration
-  def change
-  end
-end
